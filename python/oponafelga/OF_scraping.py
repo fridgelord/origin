60da147ab854c720d83ca99c9809983fb10da490
Marcin Binkowski <binkowski.mr@gmail.com>
2017-11-26 11:10:28 +0100

    v0.1 of OF_scraping.py

--- python/oponafelga/OF_scraping.py
+++ python/oponafelga/OF_scraping.py
@@ -1,26 +1,53 @@
 # coding: utf-8
 # sciaganie danych z oponafelga.pl
 
+############ TODO ###########
+## pelny profil
+## rozmiar terenowe ??X(?)?.??R??
+## rozmiar terenowe ??/(?)?.??R??
+## LI wyrzucic nawiasy, 0 na pocz (np. 099/097), XL do XL, SI do SI, wyrzucic puste
+## SI wyrzucic nawiasy, LI do LI, ZR(Y) i ZRY -> Y, male litery na duze
+## dlaczego nie ma sezonu calorocznych?
+## tireType runflat???
+## producer duze litery tylko
+## RBP wyrzucic -, 2016, C, FR
+## RBP TAK, Tak, Rant ochronny -> True
+## RPB XL, XL/RF -> XL
+## RPB ROF, RF -> runflat
+## addFeature RF, RUNFLAT, SSR, ZP - runflat, tak RUNFLAT -> runflat
+## XL LISI -> LI + SI
+## XL C, C 8PR, DOT out
+## XL Osobowe, SUV, Van/Dostawcze > type
+## XL RF -> XL
+############################
+
 import pandas as pd
 import numpy as np
 from time import sleep
 import re
 import datetime
 import os
+import shutil
+import getpass
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
 # from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.action_chains import ActionChains
 
+def isLISI(string):
+    string = string.strip()
+    return re.search('\d\d\d?/?\d?\d?\d?\D', string).group(0) == string
 
+print(isLISI('101/Y'))
+
+
+user = getpass.getuser()
+chromePath = shutil.which('chromedriver')
 options = webdriver.ChromeOptions()
-options.add_argument('headless')
-driver = webdriver.Chrome(
-    '/usr/local/bin/chromedriver', chrome_options=options)
-   # Optional argument, if not specified will search path.
-
-# driver = webdriver.Chrome()  Optional argument, if not specified will search path.
+if user == 'user':
+    options.add_argument('headless')
+driver = webdriver.Chrome(chromePath, chrome_options=options)
 
 
 def getproductsFromPage(listaOpon, dzis):
@@ -30,22 +57,31 @@ def getproductsFromPage(listaOpon, dzis):
     pageSource = driver.page_source
 
     soup = BeautifulSoup(pageSource, 'html.parser')
-    products = soup.findAll(True, {'class': 'prod-thumb prod-thumb-l '})
+    # with open('datasets/bs', 'w') as tf:
+        # tf.write(str(soup))
+        # tf.close()
+    products = soup.findAll(True, {'class': 'boxRight'})
     for prod in products:
         if (1 == 1):
             try:
-                link = re.search('href=\"(.+?)\" title',
+                link = re.search('href=\"\.\.(.+?)\"><span class=\"productClass',
                                  str(prod).replace("\n", "")).group(1)
             except:
                 print('sprawdz brak linka')
                 link = '_n/a'
             try:
-                title = re.search('\" title=\"(.+?)\">',
+                size = re.search('\"productParams\">(.+?)</span>',
                                   str(prod).replace("\n", "")).group(1)
+                brand = re.search('\"productName\">(.+?)<span>',
+                                  str(prod).replace("\n", "")).group(1)
+                pattern = re.search('\"productName\">(.+?)<span>(.+?)</span>',
+                                  str(prod).replace("\n", "")).group(2)
+                title = size+' '+brand+pattern
             except:
                 title = '_n/a'
+                print('sprawdz brak tytulu')
             try:
-                price = re.search('current-price\">(.+?) zł<',
+                price = re.search('class=\"value\">(.+?) zł<',
                                   str(prod).replace("\n", "")).group(1)
                 price = price.replace(chr(160), '')
                 price = price.replace(chr(32), '')
@@ -54,20 +90,31 @@ def getproductsFromPage(listaOpon, dzis):
             except:
                 price = np.nan
                 print("sprawdz brak ceny", link)
+            if str(prod).find('Towar dostępny od ręki') != -1:
+                delivery='24h'
+            elif str(prod).find('Czas dostawy') != -1:
+                try:
+               	    delivery = re.search('Czas dostawy (.+?) dni</span>', str(prod).replace("\n", "")).group(1)
+                except:
+                    pass
+            else:
+                delivery="nieznany"
+                print("sprawdz nowy rodzaj dostawy", title)
             try:
                 listaOpon.append([
                     link,
                     title,
                     price,
+                    delivery,
                     dzis
                 ])
             except:
                 print("cos nie tek z dodawaniem do listy", title)
     try:
-        element = driver.find_element_by_link_text('następna »')
-        klasa = element.get_attribute('class')
-        if klasa != 'jqs-pgn jqs-next pgn':
-            raise
+        element = driver.find_element_by_link_text('następna strona')
+        # klasa = element.get_attribute('class')
+        # if klasa != 'jqs-pgn jqs-next pgn':
+            # raise
         driver.execute_script("arguments[0].scrollIntoView();", element)
         driver.execute_script("window.scrollBy(0,-250)", "")
         actions = ActionChains(driver)
@@ -85,23 +132,28 @@ def getproductsFromPage(listaOpon, dzis):
 listaOpon = []
 now = datetime.datetime.now()
 dzis = now.isoformat()[:10]
-adres = 'https://sklep.intercars.com.pl/szukaj/opony-i-felgi-2/'
-try:
-    driver.get(adres)
-except:
-    print("Nie udało się otworzyć adresu", adres)
-sleep(3)
-getproductsFromPage(listaOpon, dzis)
+adresPocz = 'https://oponafelga.pl/szukaj/?marka=&rozmiar=&sezon='
+adresKonc = '&pojazd=#results'
+sezony = ['zimowe', 'letnie', 'caloroczne']
+# sezony = ['caloroczne']
+for i in sezony:
+    adres = adresPocz+i+adresKonc
+    try:
+        driver.get(adres)
+    except:
+        print("Nie udało się otworzyć adresu", adres)
+    sleep(3)
+    getproductsFromPage(listaOpon, dzis)
 
 lista_Opon = pd.DataFrame(
-    listaOpon, columns=['link', 'title', 'price', 'dateRetrieved'])
-sciezka = 'datasets/pricesIC.csv'
+    listaOpon, columns=['link', 'title', 'price', 'delivery', 'dateRetrieved'])
+sciezka = 'datasets/pricesOF.csv'
 if os.path.exists(sciezka):
     lista_Opon.to_csv(sciezka, sep=';', decimal=',', mode='a', header=False)
 else:
     lista_Opon.to_csv(sciezka, sep=';', decimal=',')
 try:
-    sciezka2 = '/mnt/scraping/pricesIC.csv'
+    sciezka2 = '/mnt/scraping/pricesOF.csv'
     if os.path.exists(sciezka2):
         lista_Opon.to_csv(
             sciezka2, sep=';', decimal=',', mode='a', header=False)
@@ -112,215 +164,92 @@ except:
 
 
 
-
-biezacaBazaOpon = pd.read_csv(
-    'datasets/tireDataIC.csv', dtype='str', header=0, sep=';', usecols=[1])
-biezacaBazaOpon = biezacaBazaOpon['link'].values.tolist()
+try:
+    biezacaBazaOpon = pd.read_csv(
+        'datasets/tireDataOF.csv', dtype='str', header=0, sep=';', usecols=[1])
+    biezacaBazaOpon = biezacaBazaOpon['link'].values.tolist()
+except:
+    biezacaBazaOpon = []
+    print ('brak bazy opon, tworze nowa')
 daneOpon = []
-# for referencja in [['/produkty/1578739-pirelli-scorpion-atr-32555-r22-116-h']]:
+# for referencja in [['/opona/vredestein_quatrac_5_59371.html']]:
 for referencja in listaOpon:
     odnosnik = referencja[0]
     if odnosnik not in biezacaBazaOpon:
         try:
-            adres = 'https://sklep.intercars.com.pl' + odnosnik
+            adres = 'https://oponafelga.pl' + odnosnik
             driver.get(adres)
             sleep(2)
         except:
-            print("Nie udało się otworzyć strony dla", odnosnik)
+            print("Nie udało się otworzyć strony dla", adres)
+            continue
         pageSource = driver.page_source
         soup = BeautifulSoup(pageSource, 'html.parser')
-        try:
-            productInfo = soup.findAll(True, {'class': 'tab-content'})
-        except:
-            print("Nie udało się znaleźć klasy tab-content dla", odnosnik)
-        soup = productInfo
-        soup = str(soup)
-        try:
-            szer = re.search(
-                '<td>Szerokość( \[mm\])?</td>\n<td class=\"jqs-cf-value\">(.+?)</td>',
-                soup).group(2)
-        except:
-            szer = '_n/a'
-        try:
-            profil = re.search(
-                '<td>Profil</td>\n<td class=\"jqs-cf-value\">(.+?)</td>',
-                soup).group(1)
-        except:
-            profil = '_n/a'
-        try:
-            osadzenie = re.search(
-                '<td>Średnica( \[mm\])?</td>\n<td class=\"jqs-cf-value\">(.+?)[\"<]<?/td>',
-                soup).group(2)
-        except:
-            osadzenie = '_n/a'
-        try:
-            sezon = re.search(
-                '<td>Sezon</td>\n<td class=\"jqs-cf-value\">(.+?)</td>',
-                soup).group(1)
-            if sezon == 'Letnia' or sezon == 'letnia':
-                sezon = 'summer'
-            elif sezon == 'zimowa':
-                sezon = 'winter'
-            elif sezon == 'całoroczna':
-                sezon = 'allseason'
-            else:
-                print('Sprawdz nowy rodzaj sezonu dla', referencja)
-                sezon = '_n/a'
-        except:
+        table = soup.find('table', attrs={'class':'produkt'})
+        table_body = table.find('tbody')
+        rows = table_body.find_all('tr')
+        tempList = []
+        for row in rows:
+            cols = row.find_all('td')
+            cols = [ele.text.strip() for ele in cols]
+            tempList.append(cols)
+        for i in [6, 5, 3, 2, 0]:
+            tempList.pop(i)
+        producent = tempList[0][0]
+        bieznik = tempList[0][1]
+        sezon = tempList[0][2].lower()
+        if sezon == 'letnie':
+            sezon = 'summer'
+        elif sezon == 'zimowe':
+            sezon = 'winter'
+        elif sezon == 'caloroczne':
+            sezon = 'allseason'
+        else:
+            print('Sprawdz nowy rodzaj sezonu dla', adres)
             sezon = '_n/a'
-        try:
-            bieznik = re.search(
-                '<td>Bieżnik</td>\n<td class=\"jqs-cf-value\">(.+?)</td>',
-                soup).group(1)
-        except:
-            try:
-                bieznik = re.search(
-                    '<td>Model bieżnika</td>\n<td class=\"jqs-cf-value\"><a class=\"accent\" href=\"(.+?)\">(.+?)</a></td>',
-                    soup).group(2)
-            except:
-                bieznik = '_n/a'
-        try:
-            zastosowanie = re.search(
-                '<td>Zastosowanie</td>\n<td class=\"jqs-cf-value\">(.+?)</td>',
-                soup).group(1)
-        except:
-            zastosowanie = '_n/a'
-        try:
-            przeznaczenie = re.search(
-                '<td>Przeznaczenie</td>\n<td class=\"jqs-cf-value\">(.+?)</td>',
-                soup).group(1)
-            if przeznaczenie == 'samochody osobowe':
-                przeznaczenie = 'CAR'
-            elif przeznaczenie == 'samochody dostawcze':
-                przeznaczenie = 'LTR'
-            elif przeznaczenie == 'samochody terenowe':
-                przeznaczenie = 'SUV/4x4'
-            else:
-                print('Sprawdz nowy rodzaj sezonu dla', referencja)
-                przeznaczenie = '_n/a'
-        except:
+        przeznaczenie = tempList[0][3]
+        if przeznaczenie == 'osobowy':
+            przeznaczenie = 'CAR'
+        elif przeznaczenie == 'dostawczy':
+            przeznaczenie = 'LTR'
+        elif przeznaczenie == '4x4':
+            przeznaczenie = 'SUV/4x4'
+        else:
+            print('Sprawdz nowy rodzaj przeznaczenia dla', adres)
             przeznaczenie = '_n/a'
+        rozmiar = tempList[1][0].replace('C','')
         try:
-            producent = re.search(
-                '<td>Producent</td>\n<td class=\"jqs-cf-value\">(.+?)</td>',
-                soup).group(1)
+            LI = re.search('(.+?) \(do', tempList[1][1]).group(1)
         except:
-            producent = '_n/a'
+            print('sprawdz brak LI dla', adres)
+            LI = ''
         try:
-            prod_kod = re.search(
-                '<td>Kod producenta</td>\n<td class=\"jqs-cf-value\">(.+?)</td>',
-                soup).group(1)
+            SI = re.search('(.+?) \(do', tempList[1][2]).group(1)
         except:
-            prod_kod = '_n/a'
-        try:
-            LI = re.search(
-                '<td>Indeks nośności</td>\n<td class=\"jqs-cf-value\">(.+?)</td>',
-                soup).group(1)
-        except:
-            LI = '_n/a'
-        try:
-            SI = re.search(
-                '<td>Indeks prędkości</td>\n<td class=\"jqs-cf-value\">(.+?) - (.+?)</td>',
-                soup).group(1)
-        except:
-            SI = '_n/a'
-        try:
-            DOT = re.search(
-                '<td>Data produkcji</td>\n<td class=\"jqs-cf-value\">(.+?)</td>',
-                soup).group(1)
-        except:
-            DOT = ''
-        try:
-            RR = re.search(
-                '<td>Opór toczenia</td>\n<td class=\"jqs-cf-value\">(.+?)</td>',
-                soup).group(1)
-        except:
-            RR = '_n/a'
-        try:
-            WG = re.search(
-                '<td>Hamowanie na mokrej nawierzchni</td>\n<td class=\"jqs-cf-value\">(.+?)</td>',
-                soup).group(1)
-        except:
-            WG = '_n/a'
-        try:
-            dB = re.search(
-                '<td>Liczba decybeli</td>\n<td class=\"jqs-cf-value\">(.+?) dB</td>',
-                soup).group(1)
-        except:
-            dB = '_n/a'
-        try:
-            noise = re.search(
-                '<td>Poziom emisji hałasu</td>\n<td class=\"jqs-cf-value\">(.+?)</td>',
-                soup).group(1)
-        except:
-            noise = '_n/a'
-        try:
-            EAN = re.search(
-                '<td>Kod EAN</td>\n<td class=\"jqs-cf-value\">(.+?)</td>',
-                soup).group(1)
-        except:
-            EAN = '_n/a'
-        try:
-            rant = re.search(
-                '<td>Rant opony</td>\n<td class=\"jqs-cf-value\">(.+?)</td>',
-                soup).group(1)
-            if 'Rant ochronny' in rant:
-                rant = 'RPB'
-            else:
-                print('Sprawdz nowy rodzaj rantu dla', referencja)
-                rant = '_n/a'
-        except:
-            rant = ''
-        runflat = False
-        try:
-            budowa = re.search(
-                '<td>Budowa opony</td>\n<td class=\"jqs-cf-value\">(.+?)</td>',
-                soup).group(1)
-            if 'runflat' in budowa.lower():
-                runflat = True
-            else:
-                print('Sprawdz nowy rodzaj rof dla', referencja)
-        try:
-            XL = re.search(
-                '<td>Nośność opony</td>\n<td class=\"jqs-cf-value\">(.+?)</td>',
-                soup).group(1)
-            if 'podwyższony' in XL:
-                XL = 'XL'
-            else:
-                print('Sprawdz nowy rodzaj XL dla', referencja)
-                XL = ''
-        except:
-            XL = ''
-        try:
-            cecha = re.search(
-                '<td>Cecha dodatkowa</td>\n<td class=\"jqs-cf-value\">(.+?)</td>',
-                soup).group(1)
-            if cecha == 'XL - podwyższony indeks nośności':
-                XL = 'XL'
-                cecha = ''
-        except:
-            cecha = ''
-        try:
-            klasa = re.search(
-                '<td>Klasa opony</td>\n<td class=\"jqs-cf-value\">(.+?)</td>',
-                soup).group(1)
-            if klasa == 'średnia':
-                klasa = 'medium'
-            elif klasa == 'premium':
-                True
-            elif klasa == 'budżet':
-                klasa = 'budget'
-            else:
-                print('Sprawdz nowy rodzaj tieru dla', referencja)
-                klasa = '_n/a'
-        except:
-            klasa = '_n/a'
-        try:
-            indeks = re.search(
-                '<td>Indeks</td>\n<td class=\"jqs-cf-value\">(.+?)</td>',
-                soup).group(1)
-        except:
-            indeks = '_n/a'
+            print('sprawdz brak SI dla', adres)
+            SI = ''
+        XL = tempList[1][3]
+        rant = tempList[2][0]
+        homol = tempList[2][1]
+        cecha = tempList[2][2]
+        prod_kod = tempList[2][3]
+
+        szer = rozmiar
+        profil = ''
+        osadzenie = ''
+        zastosowanie = ''
+        RR = ''
+        WG = ''
+        dB = ''
+        noise = ''
+        EAN = ''
+        indeks = ''
+        runflat = ''
+        DOT = ''
+        klasa = ''
+
+
+
         daneOpon.append([
             odnosnik, szer, profil, osadzenie, LI, SI, sezon, bieznik,
             zastosowanie, przeznaczenie, RR, WG, dB, noise, producent,
@@ -344,10 +273,10 @@ def zapiszDfDoCsv(dataFr, sciezka):
     else:
         dataFr.to_csv(sciezka, sep=';', decimal=',')
 
-sciezka = 'datasets/tireDataIC.csv'
-sciezkaNewTires = 'datasets/tireDataICNew.csv'
-sciezkaRemote = '/mnt/scraping/tireDataIC.csv'
-sciezkaRemoteNewTires = '/mnt/scraping/tireDataICNew.csv'
+sciezka = 'datasets/tireDataOF.csv'
+sciezkaNewTires = 'datasets/tireDataOF.csv'
+sciezkaRemote = '/mnt/scraping/tireDataOF.csv'
+sciezkaRemoteNewTires = '/mnt/scraping/tireDataOF.csv'
 listaSciezek = [sciezka, sciezkaNewTires, sciezkaRemote, sciezkaNewTires]
 for i in listaSciezek:
     try:
